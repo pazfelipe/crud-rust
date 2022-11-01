5c510354b2a7d2caac2363ab42f2573c16c88a90
Haohmaru <xxfelipepaz@gmail.com>
2022-11-01 16:28:06 -0300

    feat: create new routes

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,40 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn home() -> String {
-    "Hello, world!".to_string()
+#[macro_use]
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate serde_derive;
+
+use rocket_contrib::json::{Json, JsonValue};
+
+mod hero;
+use hero::Hero;
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+    hero
 }
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+#[get("/")]
+fn read() -> Json<JsonValue> {
+    Json(json!(["hero 1", "hero 2"]))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<JsonValue> {
+    Json(json!({"status": "ok"}))
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![home])
-        .mount("/hello", routes![hello])
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
         .launch();
 }
