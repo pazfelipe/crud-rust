709243920fe6167c605fdc8c22669fbfb96f9d08
Haohmaru <xxfelipepaz@gmail.com>
2022-11-01 16:13:32 -0300

    feat: create initial routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn home() -> String {
+    "Hello, world!".to_string()
+}
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![home])
+        .mount("/hello", routes![hello])
+        .launch();
 }
